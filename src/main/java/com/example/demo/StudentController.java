6ccba4e96cdff1c9bef752c082b418f6943a3fae
Ryan.Wang <ranwang1995@outlook.com>
2020-04-11 23:08:55 +0800

    [WangRan] feat: add method to query all students

--- src/main/java/com/example/demo/StudentController.java
+++ src/main/java/com/example/demo/StudentController.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,4 +33,13 @@ public class StudentController {
             return "没有找到名为" + name + "的学生";
         }
     }
+
+    @GetMapping("/all")
+    public String getAllStudent() {
+        Iterable<Student> allStudentIter = studentRepository.findAll();
+        StringBuilder result = new StringBuilder();
+        allStudentIter.forEach(student -> result.append(student.toString()).append("\n"));
+        return result.toString();
+    }
+
 }
